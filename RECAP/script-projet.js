572b1c77667e6b6ed16efbc06d2ce3111c52b58c
AlicyaBernos <alicyabernos@gmail.com>
2026-01-30 15:52:43 +0100

    Improve code readability and add comments
    Refactor code for clarity and maintainability, including comments for better understanding.

--- RECAP/script-projet.js
+++ RECAP/script-projet.js
@@ -42,32 +42,35 @@ function afficherDepenses() {
         btnSupprimer.addEventListener("click", function () {
             supprimerDepense(i);
         });
-
+// ici on vient  créez une case (td) pour le bouton
         const tdAction = document.createElement("td");
         tdAction.appendChild(btnSupprimer);
-
+// On remplit la ligne avec les infos donner dans le formulaire
         ligne.innerHTML =
             "<td>" + depenses[i].description + "</td>" +
             "<td>" + depenses[i].montant + " €</td>";
-
+// On colle la case du bouton dans la ligne
         ligne.appendChild(tdAction);
+    // On colle la ligne finie dans le tableau 
         tableau.appendChild(ligne);
     }
 }
-
+// ici on vient créez une variable pour stocker la somme de toutes les dépenses
 function calculerTotal() {
     let total = 0;
 
-    for (let i = 0; i < depenses.length; i++) {
-        total += depenses[i].montant;
-    }
-
+    for (let i = 0; i < depenses.length; i++) { // la boucle  parcourt toutes les dépenses
+total += depenses[i].montant; // on ajoute le montant de la dépense actuelle à total
+}
+// on affiche le total dans le div avec l'id "total"
+// innerText met le texte visible, ici "Total : [valeur] €"
     document.getElementById("total").innerText =
         "Total : " + total + " €";
 }
 
-function supprimerDepense(index) {
-    depenses.splice(index, 1);
-    afficherDepenses();
-    calculerTotal();
+function supprimerDepense(index) { 
+    depenses.splice(index, 1);// supprime 1 élément à la position 'index' dans le tableau depense
+ 
+    afficherDepenses();// ça met à jour le tableau affiché pour refléter la suppression
+    calculerTotal();//  recalcule le total après suppression pour qu'il soit correct
 }
